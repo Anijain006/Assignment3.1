9cc972b4e0ba71d111a634b87b005b7ca27861e1
Animesh Jain <anijain.0@gmail.com>
2017-10-09 15:50:34 +0530

    Initial commit

---
+++ app/src/main/java/com/animeshjain/assignment3_1/Rainbow_Spectrum.java
@@ -0,0 +1,13 @@
+package com.animeshjain.assignment3_1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Rainbow_Spectrum extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rainbow__spectrum);
+    }
+}
